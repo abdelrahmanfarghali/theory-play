7abc0475c82b50e0b79b13919ad9fdc83a4de170
Abdelrahman Amer <darkchaker@gmail.com>
2020-04-27 13:21:20 +0200

    New game, Catch The Box

---
+++ CatchtheBox/Python3.x/theory.py
@@ -0,0 +1,92 @@
+import time as tie
+start = tie.time()
+print('A')
+letter = input()
+if letter == 'a' or letter = 'A':
+    end = tie.time()
+    if end - start > 900:
+        print('timeout')
+    else:
+        print(end - start)
+else:
+    print('lost')
+print('n')
+letter = input()
+if letter == 'N' or letter = 'n':
+    end = tie.time()
+    if end - start > 900:
+        print('timeout')
+    else:
+        print(end - start)
+else:
+    print('lost')
+print('A')
+letter = input()
+if letter == 'a' or letter = 'A':
+    end = tie.time()
+    if end - start > 900:
+        print('timeout')
+    else:
+        print(end - start)
+else:
+    print('lost')
+print('p')
+letter = input()
+if letter == 'P' or letter = 'p':
+    end = tie.time()
+    if end - start > 900:
+        print('timeout')
+    else:
+        print(end - start)
+else:
+    print('lost')
+print('p')
+letter = input()
+if letter == 'P' or letter = 'p':
+    end = tie.time()
+    if end - start > 900:
+        print('timeout')
+    else:
+        print(end - start)
+else:
+    print('lost')
+print('l')
+letter = input()
+if letter == 'L' or letter = 'l':
+    end = tie.time()
+    if end - start > 900:
+        print('timeout')
+    else:
+        print(end - start)
+else:
+    print('lost')
+print('e')
+letter = input()
+if letter == 'E' or letter = 'e':
+    end = tie.time()
+    if end - start > 900:
+        print('timeout')
+    else:
+        print(end - start)
+else:
+    print('lost')
+print('MVP')
+letter = input()
+if letter == 'Apple' or letter = 'apple':
+    end = tie.time()
+    if end - start > 900:
+        print('timeout')
+    else:
+        print(end - start)
+else:
+    print('lost')
+print('H')
+letter = input()
+if letter == 'h' or letter = 'H':
+    end = tie.time()
+    if end - start > 600:
+        print('timeout')
+    else:
+        print(end - start)
+else:
+    print('lost')
--- README.md
+++ README.md
@@ -5,3 +5,5 @@ Is a game where you have to solve riddles to reach to the nth door.
 **Speed Talk**
 Is to have reflex to some words and is to answer the riddle in a specific time with the words you noticed written into the riddle.
 It has some like sentence that contains very noticable un homogeneous words into the riddle these are the answer to the question it is supposed to have no end and is to depend on a single sentence that will change order and prepositions with inserting the answer every time it changes so it can be noticable.
+**Catch the Box**
+Is to have to write down the letter of the box in few milliseconds in order to win the box, collecting the boxes you have to write down the full word in order to pick up the extreme supply box.
